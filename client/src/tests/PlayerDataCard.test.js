4ed2eb22170ff7957dabe3d0b539c86efecf1883
Matthew Bolton <matthewwbolton@gmail.com>
2020-03-13 12:32:21 -0400

    setup testing environment and started writing tests for DOM elements

--- client/src/tests/PlayerDataCard.test.js
+++ client/src/tests/PlayerDataCard.test.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, getByDisplayValue } from '@testing-library/react';
 import PlayerDataCard from '../components/PlayerDataCard';
 
 test('does the PlayerDataCard component render on the page', () => {
     render(<PlayerDataCard />);
 });
+
+test('header elements render on the page', () => {
+    const { findByText } = render(<PlayerDataCard />);
+
+    const h2 = findByText(/player name:/i);
+    const h3 = findByText(/player country:/i);
+    const h4 = findByText(/number of searches:/i);
+
+});
+
+test(`how many divs are rendered on the page`, () => {
+    const { queryAllByText } = render(<PlayerDataCard />);
+
+    const divs = queryAllByText(/fifa code:/i);
+})
--- client/src/tests/WorldCupTeams.test.js
+++ client/src/tests/WorldCupTeams.test.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, findByText, queryAllByText } from '@testing-library/react';
 import WorldCupTeams from '../components/WorldCupTeams';
 
 test('does the WorldCupTeams component render on the page', () => {
     render(<WorldCupTeams />);
 });
+
+test('do header elements render on the page', () => {
+    
+    const { findByText } = render(<WorldCupTeams />);
+
+    const h1 = findByText(/country:/i);
+    const h2 = findByText(/fifa code:/i);
+    const h3 = findByText(/group id:/i);
+    const h3GroupLetter = findByText(/group letter:/i);
+    
+
+});
+
+test('how many divs are rendered on the page', () => {
+    const { queryAllByText } = render(<WorldCupTeams />);
+
+    const divs = queryAllByText(/country:/i);
+
+})
